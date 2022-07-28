00aaf7be917b3e0fa2b9661426e250d175ef2724
citymer <cityhunter8484@gmail.com>
2022-07-28 15:34:08 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Apropos from './pages/Apropos'
+import Home from './pages/Home'
 
 const App = () => {
-  return <h1>kasa</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apropos" element={<Apropos />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/Apropos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Apropos = () => {
+  return (
+    <div>
+      <h1>apropos</h1>
+    </div>
+  )
+}
+
+export default Apropos
