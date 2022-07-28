5758edc8ecf4ab5b549709f8d8fb4622aa95e310
citymer <cityhunter8484@gmail.com>
2022-07-28 16:25:32 +0200

    creation dossier assets

--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigation from '../components/Navigation'
 
 const Apropos = () => {
   return (
     <div>
+      <Navigation />
       <h1>apropos</h1>
     </div>
   )
