40840615ac1bf942d1bfb938468aa86040865ba7
citymer <cityhunter8484@gmail.com>
2022-08-11 13:40:58 +0200

    carousel fonctionnel

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -3,20 +3,61 @@ import precedent from '../assets/precedent.png'
 import suivant from '../assets/suivant.png'
 
 const Carroussel = (logement) => {
-  console.log(logement)
+  const maison = logement.logement
+  let photoCourante = 0
+  let photoSuivante =
+    photoCourante === maison?.length - 1 ? 0 : photoCourante + 1
+  let photoPrecedente =
+    photoCourante === 0 ? maison?.length - 1 : photoCourante - 1
+
+  function nextImg(suivant) {
+    const imgAppartement = document.getElementById('appartement')
+    const positionImg = document.getElementById('positionImg')
+
+    if (imgAppartement !== null && positionImg !== null) {
+      if (suivant) {
+        imgAppartement.setAttribute('src', maison?.[photoSuivante])
+        photoCourante = photoSuivante
+        photoSuivante =
+          photoCourante === maison?.length - 1 ? 0 : photoCourante + 1
+        photoPrecedente =
+          photoCourante === 0 ? maison?.length - 1 : photoCourante - 1
+        positionImg.textContent = photoCourante + 1 + '/' + maison?.length
+      } else {
+        imgAppartement.setAttribute('src', maison?.[photoPrecedente])
+        photoCourante = photoPrecedente
+        photoSuivante =
+          photoCourante === maison?.length - 1 ? 0 : photoCourante + 1
+        photoPrecedente =
+          photoCourante === 0 ? maison?.length - 1 : photoCourante - 1
+        positionImg.textContent = photoCourante + 1 + '/' + maison?.length
+      }
+    }
+  }
+
   return (
     <div className="carroussel">
-      <button className="precedent">
-        <img src={precedent} alt="precedent" />
-      </button>
+      {maison?.length > 1 ? (
+        <button className="precedent" onClick={() => nextImg(false)}>
+          <img src={precedent} alt="precedent" />
+        </button>
+      ) : (
+        ''
+      )}
       <img
+        id="appartement"
         className="appartement"
-        src={logement.logement[0]}
+        src={maison?.[0]}
         alt="appartement"
       />
-      <button className="suivant">
-        <img src={suivant} alt="suivant" />
-      </button>
+      {maison?.length > 1 ? (
+        <button className="suivant" onClick={() => nextImg(true)}>
+          <img src={suivant} alt="suivant" />
+        </button>
+      ) : (
+        ''
+      )}
+      <p id="positionImg">{photoCourante + 1 + '/' + maison?.length}</p>
     </div>
   )
 }
