f46e9174f81b652aa6c1538774db1a00c7e99146
citymer <cityhunter8484@gmail.com>
2022-08-13 10:49:12 +0200

    comment

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -3,7 +3,7 @@ import precedent from '../assets/precedent.png'
 import suivant from '../assets/suivant.png'
 
 const Carroussel = (logement) => {
-  //tableau d'image
+  //tableau d'images
   const maison = logement.logement
   let photoCourante = 0
   let photoSuivante =
